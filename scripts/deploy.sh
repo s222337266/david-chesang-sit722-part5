3bc8083063e69bee4587dd738b6bd0bdc5325a77
s222337266 <s222337266@deakin.edu.au>
2024-10-01 23:08:12 +0300

    Update docker and add terraform files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,5 @@
-# Your Solution
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+envsubst < ./scripts/kubernetes/deploy.yaml | kubectl apply -f -
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,1 +1,10 @@
-# Your Solution
+# Push the image that has been built in build-image.sh
+    set -u # or set -o nounset
+    : "$CONTAINER_REGISTRY"
+    : "$VERSION"
+    : "$REGISTRY_UN"
+    : "$REGISTRY_PW"
+
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+docker push $CONTAINER_REGISTRY/book-catalog:$VERSION
+docker push $CONTAINER_REGISTRY/inventory-management:$VERSION
